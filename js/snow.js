2b63035928bb69a00aac57463cab11fc2bf13d7e
namelessriver <xiao16194@gmail.com>
2023-12-04 18:06:28 +0100

    Site updated: 2023-12-04 18:06:27

--- js/snow.js
+++ js/snow.js
@@ -48,7 +48,7 @@ SakuraList.prototype.update = function () {
     }
 }
 SakuraList.prototype.draw = function (cxt) {
-    for (var i = 0, len = this.list.length; i < len; i++) {
+    for (var i = 0, len = this.list.length; i < 20; i++) {
         this.list[i].draw(cxt);
     }
 }
